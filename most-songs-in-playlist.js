19cd0231d42aaa463242f140cd23f318c290481d
Krista Marbury <laetaiustus@gmail.com>
2021-08-14 14:11:33 -0500

    updates

--- most-songs-in-playlist.js
+++ most-songs-in-playlist.js
@@ -2,7 +2,7 @@
 // Each number represents the duration of a song in minutes
 // Return the maximum number of songs from the list that can fit in a 60 minute playlist
 
-var mostSongsInPlaylist = function (arr) {
+let mostSongsInPlaylist = function (arr) {
   var sortedArr = arr.sort();
   var totalMins = 0;
   var totalSongs = 0;
